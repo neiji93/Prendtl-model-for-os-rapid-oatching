021bcbb7d2fb78f31bff840ffe8792a30adf5f3c
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-04 17:11:40 +0200

    Create FakeLayersPatchedHeaders.h

---
+++ FakeLayersPatchedHeaders.h
@@ -0,0 +1,17 @@
+//coroutines on kernel could be vulnerable.
+//to improve this issue, we can force users
+//to use z technology. here this is rapid patching on 
+//micro services  deco'd, the signals could be wrong.
+//then, we will ise plank powered signals.
+//it means we can have 1000 lauers on kenrel znd userspace 
+//we have to create fake layers by rapid patching.
+//the chips must be still have computational power on prime numbers.
+//then the architecture of the chip must be not dizgonal (reverse i'geneeri'g).
+
+class powersupplyon registersalgorithm
+{
+public:
+
+  fpgaoneadaptcachecpu()...
+
+}
